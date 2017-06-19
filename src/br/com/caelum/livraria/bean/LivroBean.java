ec5bf1ccf73217ada6a4661c240cd67ddacceb29
glerison <glerison@DESKTOP-N4N1AT2>
2017-06-19 01:29:26 -0300

    enviando projeto

---
+++ src/br/com/caelum/livraria/bean/LivroBean.java
@@ -0,0 +1,85 @@
+package br.com.caelum.livraria.bean;
+
+import java.util.List;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+
+import br.com.caelum.livraria.dao.DAO;
+import br.com.caelum.livraria.modelo.Autor;
+import br.com.caelum.livraria.modelo.Livro;
+
+@ManagedBean
+@ViewScoped
+public class LivroBean {
+
+	private Livro livro = new Livro();
+	private Integer autorId;
+
+	public void setAutorId(Integer autorId) {
+		this.autorId = autorId;
+	}
+
+	public Integer getAutorId() {
+		return autorId;
+	}
+
+	public Livro getLivro() {
+		return livro;
+	}
+
+	public List<Autor> getAutores() {
+		return new DAO<Autor>(Autor.class).listaTodos();
+	}
+
+	public void gravarAutor() {
+		Autor autor = new DAO<Autor>(Autor.class).buscaPorId(this.autorId);
+		this.livro.adicionaAutor(autor);
+	}
+	
+
+	public void gravar() {
+		System.out.println("Gravando livro " + this.livro.getTitulo());
+
+		if (livro.getAutores().isEmpty()) {
+			//throw new RuntimeException("Livro deve ter pelo menos um Autor.");
+			FacesContext.getCurrentInstance().addMessage("autor", new FacesMessage("Livro deve ter pelo menos um Autor."));
+			return;
+		}
+		if(this.livro.getId() == null){
+			new DAO<Livro>(Livro.class).adiciona(this.livro);
+		}else{
+			new DAO<Livro>(Livro.class).atualiza(this.livro);
+		}
+		this.livro = new Livro();
+	}
+	
+	public void remover(Livro livro){
+		System.out.println("Removendo livro...");
+		new DAO<Livro>(Livro.class).remove(livro);
+	}
+	
+	public void removerAltorLivro(Autor autor){
+		this.livro.removeAutor(autor);
+	}
+	
+	public String formAutor(){
+		System.out.println("Chamando cadastro de autor!");
+		return "autor?faces-redirect=true";
+	}
+
+	public List<Autor> getAutoresDoLivro() {
+		return this.livro.getAutores();
+	}
+	
+	public List<Livro> getLivros(){
+		return new DAO<Livro>(Livro.class).listaTodos();
+	}
+	
+	public void carregar(Livro livro){
+		this.livro = livro;
+	}
+
+}
